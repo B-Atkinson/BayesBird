cbc95118e3351af2c3c5c2419b5b7cda5303ac25
brian.atkinson <brian.atkinson@nps.edu>
2022-08-08 11:34:16 -0700

    fixed initialization error on MAC

--- submitXie.sh
+++ submitXie.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-source activate torch
-OUTPUT=/Users/student/Documents/brian/data/TempTest/
+conda activate torch
+OUTPUT=/Users/student/Documents/brian/data/LoopTest/
 NUM=20000
 HIDDENS=4
 JOB=0
 SEED=1
 
-for TEMP in 100
+for TEMP in 1e-8 1e-7 1e-6 1 10 100
 do
 
-screen -dmS TEST python FBmain.py \
+screen -dm python FBmain.py \
 --seed=$SEED \
 --output_dir=$OUTPUT \
 --num_episodes=$NUM \
