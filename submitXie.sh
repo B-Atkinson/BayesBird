0f8501dde95e2ea73acdb22f40c211ae8cc6a23d
brian.atkinson <brian.atkinson@nps.edu>
2022-08-19 10:23:41 -0700

    updated with new params

--- submitXie.sh
+++ submitXie.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 conda activate torch
-OUTPUT=/Users/student/Documents/brian/data/CNNTest/
-NUM=30000
+OUTPUT=/Users/student/Documents/brian/data/DiscountTest_2/
+NUM_EPS=30000
 JOB=0
 SEED=1
-HIDDENS=3
+HIDDENS=4
 
-for LR in .001 .0001 .00001
+for LR in .0001 .00001
 do
 
 screen -dm python /Users/student/Documents/brian/BayesBird/FBmain.py \
 --model_type=CNN_PG \
 --seed=$SEED \
 --output_dir=$OUTPUT \
---num_episodes=$NUM \
+--num_episodes=$NUM_EPS \
 --num_hiddens=$HIDDENS \
---batch_size=30 \
+--batch_size=100 \
 --L2=.0001 \
 --learning_rate=$LR \
 --leaky=false \
