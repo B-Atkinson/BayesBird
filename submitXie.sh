9fabe8707bbfe1a5248c692af1f275ecdc0dbb06
brian.atkinson <brian.atkinson@nps.edu>
2022-08-29 15:56:58 -0700

    ran MLP test

--- submitXie.sh
+++ submitXie.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 conda activate torch
-OUTPUT=/Users/student/Documents/brian/data/PenaltyTestSearch/
+OUTPUT=/Users/student/Documents/brian/data/MLPTest/
 NUM_EPS=30000
 JOB=0
 SEED=1
 HIDDENS=4
 
-for LR in .00001
+for LR in 1e-4 1e-5
 do
-    for L2 in .01 .001
+    for L2 in .001
     do
         screen -dm python /Users/student/Documents/brian/BayesBird/FBmain.py \
-        --model_type=CNN_PG \
+        --model_type=PGNetwork \
         --seed=$SEED \
         --output_dir=$OUTPUT \
         --num_episodes=$NUM_EPS \
         --num_hiddens=$HIDDENS \
         --cells=2 \
-        --batch_size=20 \
+        --batch_size=200 \
         --L2=$L2 \
         --learning_rate=$LR \
         --leaky=false \
