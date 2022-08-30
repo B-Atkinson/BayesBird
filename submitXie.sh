01513bec618a8774783f18faf22ac3398943de7b
brian.atkinson <brian.atkinson@nps.edu>
2022-08-30 15:43:43 -0700

    added weight init param

--- submitXie.sh
+++ submitXie.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 conda activate torch
-OUTPUT=/Users/student/Documents/brian/data/MLPTest/
+OUTPUT=/Users/student/Documents/brian/data/DeepMLPTest/
 NUM_EPS=30000
 JOB=0
 SEED=1
 
-for LR in 1e-4 1e-5
+for LR in 1e-5 1e-6
 do
-    for HIDDENS in 2 3 4
+    for HIDDENS in 4 5
     do
-        screen -dm python /Users/student/Documents/brian/BayesBird/FBmain.py \
-        --model_type=PGNetwork \
-        --seed=$SEED \
-        --output_dir=$OUTPUT \
-        --num_episodes=$NUM_EPS \
-        --num_hiddens=$HIDDENS \
-        --cells=2 \
-        --batch_size=200 \
-        --L2=.01 \
-        --learning_rate=$LR \
-        --leaky=false \
-        --sigmoid=true \
-        --temperature=1
+        for LEAKY in true false
+        do
+            screen -dm python /Users/student/Documents/brian/BayesBird/FBmain.py \
+            --model_type=PGNetwork \
+            --seed=$SEED \
+            --output_dir=$OUTPUT \
+            --num_episodes=$NUM_EPS \
+            --init_method=He \
+            --num_hiddens=$HIDDENS \
+            --cells=2 \
+            --batch_size=200 \
+            --L2=.01 \
+            --learning_rate=$LR \
+            --leaky=$LEAKY \
+            --sigmoid=true \
+            --temperature=1
 
-        let JOB=JOB+1
+            let JOB=JOB+1
+        done
     done
 done
 
